d741736a0e54b64c72353361e569fdcb95bc1a12
AmadorDev <amadorDev@gmail.com>
2022-08-10 12:05:45 -0500

    devs

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ module.exports = {
     defaultLocale: "es-ES",
   },
   images: {
-    domains: ["127.0.0.1", "127.0.0.1:6767", "localhost", "www.fillmurray.com","radiant.fabriqadigital.com","nksnprojects.com"],
+    domains: ["127.0.0.1", "127.0.0.1:6767", "localhost", "www.fillmurray.com","radiant.fabriqadigital.com","nksnprojects.com","back.placentaliferadiant.com"],
     deviceSizes: [670, 750, 828, 1080, 1224, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
